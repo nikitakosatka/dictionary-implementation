4898a6ec2d8b11e3f88643c4d5ca87ef95b382e5
nikitakosatka <nikitakosatka@yandex.ru>
2022-04-27 01:44:14 +0300

    Unordered Dictionary Implementation

---
+++ dictionary/UnorderedDictionary.h
@@ -0,0 +1,56 @@
+#ifndef DICTIONARY_IMPLEMENTATION_UNORDEREDDICTIONARY_H
+#define DICTIONARY_IMPLEMENTATION_UNORDEREDDICTIONARY_H
+
+#include <cstddef>
+#include <vector>
+#include <iterator>
+
+#include "Dictionary.h"
+#include "utils.h"
+
+
+template<class Key, class Value>
+class UnorderedDictionary : Dictionary<Key, Value> {
+private:
+    const size_t mod = HASH_MOD;
+    std::vector<std::vector<std::pair<Key, Value>>> table;
+
+    const size_t getHash(const Key &key) const {
+        return std::hash<Key>{}(key) % mod;
+    }
+
+public:
+    UnorderedDictionary() {
+        table.resize(mod);
+    }
+
+    const Value &get(const Key &key) const {
+        size_t hashValue = getHash(key);
+
+        for (size_t i = 0; i < table[hashValue].size(); i++) {
+            if (table[hashValue][i].first == key) {
+                return table[hashValue][i].second;
+            }
+        }
+
+        throw key_not_found_exception<Key>();
+    }
+
+    void set(const Key &key, const Value &value) {
+        size_t hashValue = getHash(key);
+
+        table[hashValue].push_back(std::make_pair(key, value));
+    }
+
+    bool is_set(const Key &key) const {
+        try {
+            get(key);
+            return true;
+        } catch (key_not_found_exception<Key>) {
+            return false;
+        }
+    }
+};
+
+
+#endif
