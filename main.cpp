a1790966ccd298461f7d4a8c1403ad7f0e7755dc
nikitakosatka <nikitakosatka@yandex.ru>
2022-04-27 01:44:46 +0300

    Some examples

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "dictionary/UnorderedDictionary.h"
+
+using namespace std;
+
+int main() {
+    UnorderedDictionary<string, int> dict;
+
+    dict.set("Ten", 10);
+    dict.set("One", 1);
+
+    cout << "Ten is in dict: " << dict.is_set("Ten") << endl;
+    cout << "Two is in dict: " << dict.is_set("Two") << endl;
+    cout << "Get One: " << dict.get("One") << endl;
+    cout << "Get Two: " << dict.get("Two") << endl;
+
+    return 0;
+}
